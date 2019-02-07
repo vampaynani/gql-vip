ebc7b23e2f116c06cd8238349ea318939fb07bb0
Wenceslao Negrete <vampay@digitaldealers.mx>
2019-02-07 10:31:53 -0600

    protect using context and authorization header

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const { GraphQLServer } = require('graphql-yoga');
+const DBUser = require('./models/user');
+const jwt = require('jsonwebtoken');
 
 // routers
 const typeDefs = './schema.graphql';
@@ -11,7 +13,21 @@ const Person = require('./resolvers/Person');
 // app.use(...)
 const server = new GraphQLServer({
   typeDefs,
-  resolvers: { Query, Mutation, Person }
+  resolvers: { Query, Mutation, Person },
+  context: incomingRequest => ({
+    validateUser: async () => {
+      const AuthHeader = incomingRequest.request.header('authorization');
+      if(!AuthHeader){
+        throw "Unauthorized";
+      }
+      const token = AuthHeader.replace('Bearer', '');
+      const decodedToken = jwt.verify(token, 'secretpassphrase');
+      const isUser = await DBUser.findById(decodedToken.user._id);
+      if(!isUser){
+        throw "Unauthorized";
+      }
+    }
+  })
 });
 
 // app.listen(...)
--- resolvers/query/person.js
+++ resolvers/query/person.js
@@ -1,6 +1,9 @@
 const axios = require('axios');
 
-module.exports = async (root,args) => {
- return await axios.get(`https://swapi.co/api/people/${args.id}/`)
-  .then(res => res.data); 
+module.exports = async (root,args, context) => {
+  const { validateUser } = context;
+  
+  await validateUser();
+  return await axios.get(`https://swapi.co/api/people/${args.id}/`)
+    .then(res => res.data);
 }
